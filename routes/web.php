2eca03ebdee8b5a878dd27532e98d8c589763281
Antoine-Rzepa <antoine.rzepa@le-campus-numerique.fr>
2021-02-22 13:53:57 +0100

    Iteration 2 etape 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Home page";
+});
+
+Route::get('product', function () {
+    return "Liste des produits";
+});
+
+Route::get('product/{id}', function ($id) {
+    return "Fiche du produit ".$id;
+});
+
+Route::get('cart', function () {
+    return "Panier ";
 });
